dd84bbc309b111577aeeed76a1d1a54852ecf76c
Abhinandhc <abhinandhc407@gmail.com>
2025-07-03 20:50:15 +0530

    Backend api fix

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://next-level-nexus-1npa.onrender.com",
     credentials:true
 }));
  
