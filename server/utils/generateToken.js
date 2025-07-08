69a9e3010fa27cf507979789817c84d4dbeffa81
Abhinandhc <abhinandhc407@gmail.com>
2025-07-08 23:24:09 +0530

    gneratetoken

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -6,14 +6,16 @@ export const generateToken = (res, user, message) => {
   });
 
   return res
-    .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      secure: true,           // Needed for HTTPS (e.g., Render)
+      sameSite: "None",       // Needed for cross-site cookies
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-    }).json({
-        success:true,
-        message,
-        user
+    })
+    .status(200)
+    .json({
+      success: true,
+      message: message || `Welcome back ${user.fullName}`,
+      user,
     });
 };
