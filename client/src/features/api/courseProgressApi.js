50c1f6b271756472a6f2e2efb3e8936f2ff5ceec
Abhinandhc <abhinandhc407@gmail.com>
2025-07-08 21:34:57 +0530

    some additional change

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const  COURSE_PROGRESS_API = "https://next-level-nexus.onrender.com/api/v1/progress";
+const  COURSE_PROGRESS_API = "https://next-level-nexus.onrender.com/api/v1/progress/";
 export const courseProgressApi = createApi({
     reducerPath:"courseProgressApi",
     baseQuery: fetchBaseQuery({
